b1c65b7d86bcef07b35c8b0e7dd8a4558b306ffa
bochora3000 <irakli.bochorishvili.2@gmail.com>
2025-01-18 18:02:24 +0100

    Refactored main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -21,7 +14,6 @@ def main():
         
         pygame.display.flip()
         
-
 
 if __name__ == "__main__":
     main()
